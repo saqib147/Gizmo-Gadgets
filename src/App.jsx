6f1c77bf117428824e9621f2be26250b972aae6d
Saqib Arshad <saqibarshad429@gmail.com>
2024-09-19 19:38:28 +0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { Hero } from "./customer/components/HeroSection/Hero.jsx";
 import HotProducts from "./customer/components/HotProducts/HotProducts.jsx";
 import InfiniteScroll from "./customer/components/InfiniteScroll/InfiniteScroll.jsx";
 import Navbar from "./customer/components/Navigation/Navigation.jsx";
-// import Navigation from "./customer/components/Navigation/Navigation.jsx";
 
 function App() {
   return (
